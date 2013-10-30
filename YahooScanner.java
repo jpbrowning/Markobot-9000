6cc290580fdc101657fd469816da7ba25f0b030b
James Browning <Jamboix@gmail.com>
2013-10-30 10:36:23 -0400

    Create YahooScanner.java
    This dosen't work yet. But it will.

---
+++ YahooScanner.java
@@ -0,0 +1,15 @@
+# Mark Atkins, James Browning, Eric Markovich
+# Loads a Yahoo! Finance URL and grabs the price from CSV format
+
+public class YahooScanner
+{
+   public YahooScanner()
+   {
+      
+   }
+   
+   public getPrice(symbol)
+   {
+     URL = "http://finance.yahoo.com/d/quotes.csv?s=" + symbol + "&f=sl1d1t1c1ohgv&e=.csv");
+   }
+}
