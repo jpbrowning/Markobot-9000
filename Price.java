b5764ef1deb803e6fe7ecf6fbac6428369dade21
James Browning <Jamboix@gmail.com>
2013-10-30 13:10:01 -0400

    Create Price.java
    Working as intended.

---
+++ Price.java
@@ -0,0 +1,18 @@
+public class Price {
+  public String symbol;
+	public double value;
+	public String date;
+	public String time;
+	
+	public Price(String s, double v, String d, String t){
+		this.symbol = s;
+		this.value = v;
+		this.date = d;
+		this.time = t;
+	}
+	
+	public String toString(){
+		return (symbol + " " + value + " " + date + " " + time);
+	}
+
+}
