e3f23c650dc7faf1ba09713d5543ddfae56ba9e6
jpbrowning <jamboix@gmail.com>
2013-11-26 10:03:27 -0500

    Merge

--- src/YahooScanner.java
+++ src/YahooScanner.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Calendar;
-import java.util.Date;
 
 // Mark Atkins, James Browning, Eric Markovich
 // Loads a Yahoo! Finance URL and grabs the price from CSV format
